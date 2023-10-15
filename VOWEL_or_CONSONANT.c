b131ad9e6172eaa48f6eafeb5bde9d55fb2e9c5c
varnikakarri_30 <147399390+Varnikakarri30@users.noreply.github.com>
2023-10-15 09:26:06 -0700

    #include <stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch==65||ch==69||ch==73||ch==79||ch==85||ch==97||ch==101||ch==105||ch==111||ch==117 )     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch==65||ch==69||ch==73||ch==79||ch==85||ch==97||ch==101||ch==105||ch==111||ch==117 )
+    {
+        printf("VOWEL");
+    }
+    else 
+    {
+        printf("CONSONANT");
+    }
+}
+
+
+    
+    
+    
+
+
