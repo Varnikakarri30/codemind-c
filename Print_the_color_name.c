9f96e2016c1b172a76d7484b3cb089d8978c78fa
varnikakarri_30 <147399390+Varnikakarri30@users.noreply.github.com>
2023-10-15 21:49:09 -0700

    #include <stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch==85)     {         printf("Violet");     }     else if(ch==73)     {         printf("Indigo");     }     else if(ch==66)     {         printf("Blue");     }     else if(ch==71)     {         printf("Green");     }     else if(ch==89)     {         printf("Yellow");     }     else if(ch==79)     {         printf("Orange");     }     else if(ch==82)     {         printf("Red");     }     else     {         printf("-1");     }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch==85)
+    {
+        printf("Violet");
+    }
+    else if(ch==73)
+    {
+        printf("Indigo");
+    }
+    else if(ch==66)
+    {
+        printf("Blue");
+    }
+    else if(ch==71)
+    {
+        printf("Green");
+    }
+    else if(ch==89)
+    {
+        printf("Yellow");
+    }
+    else if(ch==79)
+    {
+        printf("Orange");
+    }
+    else if(ch==82)
+    {
+        printf("Red");
+    }
+    else 
+    {
+        printf("-1");
+    }
+
+
+    
+    
+    
+}
