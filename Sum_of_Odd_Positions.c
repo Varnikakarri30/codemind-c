76e461e78553a1444e9d86fe8922060b4540f7cf
varnika_karri <147399390+Varnikakarri30@users.noreply.github.com>
2023-12-19 11:26:48 +0530

    #include <stdio.h> int main() {     int n,i,sum=0,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=1;i<n;i+=2)     {         sum=sum+arr[i];     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,sum=0,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i+=2)
+    {
+        sum=sum+arr[i];
+    }
+    printf("%d",sum);
+}
